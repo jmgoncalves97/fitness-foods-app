85684c3f1558fcf381e2dee35755ced5e7fdb366
Jonathan Gonçalves <jonathanmattosgoncalves1@gmail.com>
2024-11-09 22:33:28 -0300

    feat: add console command to dispatch OpenFoodFacts

--- sync-data/routes/console.php
+++ sync-data/routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Domain\OpenFoodFacts\Services\OpenFoodFactsService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+
+use App\Jobs\ImportOpenFoodFactsJob;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('openfoodfacts:import', function () {
+    ImportOpenFoodFactsJob::dispatch(app(OpenFoodFactsService::class));
+})->dailyAt('02:00');
